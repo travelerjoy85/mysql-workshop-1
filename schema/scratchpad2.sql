1941c7a11deced9c582f1d56af535a7e2bf896f3
Joy <travelerjoy85@gmail.com>
2017-04-10 19:44:09 +0000

    in progress

--- schema/scratchpad2.sql
+++ schema/scratchpad2.sql
@@ -1,21 +1,27 @@
 CREATE TABLE Account (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(30),
-    lastName VARCHAR(30)
+    email VARCHAR(255),
+    password VARCHAR(40),
+    createdOn DATETIME,
+    modifiedOn DATETIME
 );
 
 CREATE TABLE AddressBook (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(30),
-    lastName VARCHAR(30),
-    Address VARCHAR(100)
+    accountId INT,
+    name VARCHAR(255),
+    createdOn DATETIME,
+    modifiedOn DATETIME
 );
 
 CREATE TABLE Entry (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(30),
-    lastName VARCHAR(30),
-    entryDate DATETIME
+    addressBookId INT,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    birthday DATETIME,
+    entryType ENUM('home', 'work', 'other'),
+    subType ENUM('phone', 'address', 'email')
 );
 
 CREATE TABLE Test (
